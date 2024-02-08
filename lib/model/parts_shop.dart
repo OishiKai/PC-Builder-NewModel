def6cb53dad8b2cbd256237c8eb303284b28abf8
Kai Oishi <bokuchan.kai@gmail.com>
2024-02-08 22:45:19 +0900

    pc_partsモデル追加

---
+++ lib/model/parts_shop.dart
@@ -0,0 +1,15 @@
+class PartsShop {
+  PartsShop(
+    this.rank,
+    this.price,
+    this.bestPriceDiff,
+    this.shopName,
+    this.shopPageUrl,
+  );
+
+  final String rank;
+  final String price;
+  final String bestPriceDiff;
+  final String shopName;
+  final String shopPageUrl;
+}
